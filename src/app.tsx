db7f453a8274d41690acf8466ae5852a76328c59
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2026-03-20 21:05:40 +0200

    setup self-hosted convex for offline development

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { ConvexProvider } from "convex-solidjs";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
 import "./app.css";
 import { client } from "./integrations/convex";
 
@@ -11,7 +10,6 @@ export default function App() {
     <Router
       root={(props) => (
         <>
-          <Nav />
           <Suspense>
             <ConvexProvider client={client}>{props.children}</ConvexProvider>
           </Suspense>
--- src/integrations/convex.ts
+++ src/integrations/convex.ts
@@ -1,3 +1,3 @@
-import { ConvexProvider, setupConvex } from "convex-solidjs";
+import { setupConvex } from "convex-solidjs";
 
 export const client = setupConvex(import.meta.env.VITE_CONVEX_URL);
