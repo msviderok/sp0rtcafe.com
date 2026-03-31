2bee124b958d63996dc98bacaefba6b98ea90011
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2026-04-01 00:31:59 +0300

    fix env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { solidStart } from "@solidjs/start/config";
 import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [solidStart(), tailwindcss(), nitro()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [solidStart(), tailwindcss(), nitro()],
+    environments: {
+      ssr: {
+        define: {
+          "process.env.UPLOADTHING_TOKEN": JSON.stringify(env.UPLOADTHING_TOKEN),
+        },
+      },
+    },
+  };
 });
